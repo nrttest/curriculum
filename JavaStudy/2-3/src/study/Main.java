c294baba07215677179a110e6893b8399e1a4685
nrttest <90502357+nrttest@users.noreply.github.com>
2021-10-25 22:03:38 +0900

    Create Main.java

---
+++ JavaStudy/2-3/src/study/Main.java
@@ -0,0 +1,16 @@
+package study;
+/**
+ *
+ * 本課題では、継承・オーバーロードメソッドの基本的な使い方を学んでいきましょう。
+ * 課題は問①から問③まであります。
+ * 指定された値と変数名を守って記述してください。
+ *
+ * @author s.nanaumi
+ */
+public class Main {
+    public static void main(String[] args) {
+        Task T=new Task();
+        T.doTask();
+    }
+
+}
