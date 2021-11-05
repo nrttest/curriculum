f947dc42de8fd3e5f572578965ce1f6a528b73a1
nrttest <90502357+nrttest@users.noreply.github.com>
2021-11-05 17:58:30 +0900

    Revert "Update Task1_4.java"
    課題修正2-4

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -1,15 +1,17 @@
 package study;
 
+import constants.FruitsConstants;
+import fruits.Fruits;
 public class Study {
+
 
     public static void main(String[] args) {
         /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
          */
-        constants.FruitsConstants list=new  constants.FruitsConstants();
-        fruits.Fruits print=new fruits.Fruits();
-        print.printFruits(list.FRUITS_LEMON_04,list.FRUITS_COUNT_20);        
+     
+        Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04,FruitsConstants.FRUITS_COUNT_20);
     }
 
 }
